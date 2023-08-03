8e217a817cb1ddbf9c23d3d072b67e164e4625cd
Mehedi Hassan <mehedi.github@gmail.com>
2023-08-03 13:36:34 +0600

    login screen design done

--- app/src/main/java/com/example/opus/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/opus/ui/activities/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.opus
+package com.example.opus.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.opus.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
