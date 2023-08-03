e4b26fc22fa0c5d9e7f7467d52d3fa0ed3570b47
Mehedi Hassan <mehedi.github@gmail.com>
2023-08-03 17:52:52 +0600

    register screen design done

--- app/src/main/java/com/example/opus/ui/fragments/auth/LoginFragment.kt
+++ app/src/main/java/com/example/opus/ui/fragments/auth/LoginFragment.kt
@@ -5,18 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.opus.R
+import androidx.navigation.Navigation
+import com.example.opus.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnCreateAccount.setOnClickListener {
+            employerLogin()
+        }
+    }
+
+    private fun employerLogin() {
+
+        //Navigation.fin
     }
 
 }
